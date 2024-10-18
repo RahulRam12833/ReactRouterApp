d15b46fa3296928684cf28d9400ffb51f38d48aa
Rahul <ramr12833@gmail.com>
2024-10-18 20:00:46 +1030

    Refactored the Navbar using NavLinks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,9 @@
-import { Link ,Outlet} from "react-router-dom";
-import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <>
-    <Navbar/>
+    
     <section className='section'>
-        
-        <Outlet/>
+      <h2>Home</h2>
       </section>
     </>
      
