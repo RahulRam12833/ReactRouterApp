889644a603233c2dc1571a2fdd8ad29e30d461df
Rahul <ramr12833@gmail.com>
2024-10-21 14:26:53 +1030

    Created the products page

--- src/data.js
+++ src/data.js
@@ -3,14 +3,14 @@ const products = [
     id: 'recZkNf2kwmdBcqd0',
     name: 'accent chair',
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681749482/react-comfy-store-products/iuYyO9RP_o_upinxq.jpg',
+      'https://res.cloudinary.com/dpsteejmc/image/upload/v1681749482/react-comfy-store-products/iuYyO9RP_o_upinxq.jpg',
   },
   {
     id: 'recEHmzvupvT8ZONH',
     name: 'albany sectional',
 
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750782/react-comfy-store-products/product-2_lusrzx.jpg',
+      'https://res.cloudinary.com/dpsteejmc/image/upload/v1681750782/react-comfy-store-products/product-2_lusrzx.jpg',
   },
   {
     id: 'rec5NBwZ5zCD9nfF0',
